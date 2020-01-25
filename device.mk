e192d2e1e54e145b6b26487d4abd8b1cc8245b09
argraur <argraur@gmail.com>
2020-01-25 22:27:42 +0530

    violet: Use Pixel charger animation
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -340,6 +340,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.sm6150-libperfmgr
