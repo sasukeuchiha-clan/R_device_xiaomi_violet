2418d839c982d52e0336c4e56e654d3f635ec4e7
karthik558 <karthik.lal558@gmail.com>
2021-01-28 17:49:22 +0530

    violet: OctaviOS Bringup
    * Added eng build option on AndroidProduct.mk
    * Removed aosp.dependencies (will add later)
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- octavi_violet.mk
+++ octavi_violet.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The PixelExperience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,18 +7,22 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common OctaviOS stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_AOSP_RECOVERY := true
 IS_PHONE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_violet
+PRODUCT_NAME := octavi_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+#OCTAVI_STUFFS
+OCTAVI_BUILD_TYPE=Official
+TARGET_FACE_UNLOCK_SUPPORTED := true
+OCTAVI_DEVICE_MAINTAINER := KARTHIK.LAL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet"
