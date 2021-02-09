96b986c28369cea58d39f26d432bd341b19710bf
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    violet: Switch to source built vendor.qti.hardware.perf@2.0
    Change-Id: I1dc96e4a51c4c81dd847c59a58ec670d89054f64
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -353,7 +353,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.sm6150-libperfmgr
+    android.hardware.power-service.sm6150-libperfmgr \
+    vendor.qti.hardware.perf@2.0
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
